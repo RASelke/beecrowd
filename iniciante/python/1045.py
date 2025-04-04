6c2cd547d47383d22b120db2cc44ceab9418f193
RASelke <renan.selke70@gmail.com>
2025-04-03 23:10:48 -0300

    problems

--- iniciante/python/1045.py
+++ iniciante/python/1045.py
@@ -1,19 +1,27 @@
-entry = input().strip().split()
-entry = sorted(entry, reverse=False)
-print(entry)
+entry = input().split()
+for i in range(len(entry)):
+    entry[i] = float(entry[i])
+entry = sorted(entry, reverse=True)
+
 A = float(entry[0])
 B = float(entry[1])
 C = float(entry[2])
 
-if A >= B + C:
+if A >= (B + C):
     print("NAO FORMA TRIANGULO")
-if A*A == (B*B + C*C):
-    print("TRIANGULO RETANGULO")
-if A*A > (B*B + C*C):
-    print("TRIANGULO OBSTUSANGULO")
-if A*A < (B*B + C*C):
-    print("TRIANGULO ACUTANGULO")
-if A == B and B == C:
-    print("TRIANGULO EQUILATERO")
-if (A == B or B == C or A == C) and (A != B and B != C):
-    print("TRIANGULO ISOSCELES")
+
+else:
+    if (A*A) == (B*B + C*C):
+        print("TRIANGULO RETANGULO")
+
+    if (A*A) > (B*B + C*C):
+        print("TRIANGULO OBTUSANGULO")
+
+    if (A*A) < (B*B + C*C):
+        print("TRIANGULO ACUTANGULO")
+
+    if A == B and B == C:
+        print("TRIANGULO EQUILATERO")
+
+    if (A == B or B == C or A == C) and (A != B or B != C):
+        print("TRIANGULO ISOSCELES")
