d0e3dbd002b02a8a2481a3872a48fef92b10a0ba
RASelke <renan.selke70@gmail.com>
2025-04-04 11:01:23 -0300

    problems

---
+++ iniciante/c/1000/1020.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int N;
+    scanf("%d", &N);
+    printf("%d ano(s)\n", N / 365);
+    N = N % 365;
+    printf("%d mes(es)\n", N / 30);
+    N = N % 30;
+    printf("%d dia(s)\n", N);
+    return 0;
+}
